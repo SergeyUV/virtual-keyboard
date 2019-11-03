a324147f2cb77e0b1f97ff5828c46273b00a6ecc
Sergey Vnukov <skivun@hotmail.com>
2019-11-03 19:53:58 +0300

    feat: add language switching and tabs on tab-key

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ class cVKeyboard {
   constructor(){
     this.vKeyboardRows = 5;
     this.lang = 'en';
-    this.leftShiftPressed = 0; //0 - not pressed, 0 - pressed;
-    this.rightShiftPressed = 0; //0 - not pressed, 0 - pressed;
-    this.ctlPressed = 0;
+    this.leftShiftPressed = false; 
+    this.rightShiftPressed = false;
+    this.leftCtrlPressed = false;
     this.altPressed = 0;
+    this.caps = false;
+    this.elText;
+    const storedLang = sessionStorage.getItem('vKeyboardLang');
+    if (storedLang){
+      this.lang = storedLang;
+    }
     this.keysProp = [
       //row0
       [
@@ -237,82 +243,156 @@ class cVKeyboard {
 
       ]
     ];
-    
+
     this.keyboardLangs = 
-      { en: [['Backquote', '`', '~'], ['Digit1','1'], ['Digit2','2'], ['Digit3','3'], ['Digit4','4'], ['Digit5','5'],
-             ['Digit6','6'], ['Digit7','7'], ['Digit8','8'], ['Digit9','9'], ['Digit0','0'],
-             ['Minus','-'], ['Equal','='], ['Backspace','Backspace','Backspace'], ['Tab','Tab','Tab'], ['KeyQ','q'], ['KeyW','w'],    
+      { en: [['Backquote', '`', '~'], ['Digit1','1','!'], ['Digit2','2','@'], ['Digit3','3','#'], ['Digit4','4','$'], ['Digit5','5','%'],
+             ['Digit6','6','^'], ['Digit7','7','&'], ['Digit8','8','*'], ['Digit9','9','('], ['Digit0','0',')'],
+             ['Minus','-','_'], ['Equal','=','+'], ['Backspace','Backspace','Backspace'], ['Tab','Tab','Tab'], ['KeyQ','q'], ['KeyW','w'],    
              ['KeyE','e'], ['KeyR','r'], ['KeyT','t'], ['KeyY','y'], ['KeyU','u'], ['KeyI','i'], ['KeyO','o'],
-             ['KeyP','p'], ['BracketLeft','['], ['BracketRight',']'], ['Backslash','\\','|'], ['Delete', 'Del','Del'],
+             ['KeyP','p'], ['BracketLeft','[','{'], ['BracketRight',']','}'], ['Backslash','\\','|'], ['Delete', 'Del','Del'],
              ['CapsLock','Caps','Caps'], ['KeyA','a'], ['KeyS','s'], ['KeyD','d'], ['KeyF','f'], ['KeyG','g'], ['KeyH','h'],
-             ['KeyJ','j'], ['KeyK','k'], ['KeyL','l'], ['Semicolon',':'], ['Quote','\''], ['Enter','Enter', 'Enter'],
+             ['KeyJ','j'], ['KeyK','k'], ['KeyL','l'], ['Semicolon',';',':'], ['Quote','\'','"'], ['Enter','Enter', 'Enter'],
              ['ShiftLeft','Shift','Shift'], ['KeyZ','z'], ['KeyX','x'],['KeyC','c'], ['KeyV','v'], ['KeyB','b'], ['KeyN','n'],
              ['KeyM', 'm'], ['Comma',',','<'], ['Period','.','>'], ['Slash','/','?'], ['ArrowUp','up','up'], ['ShiftRight', 'Shift','Shift'],
-             ['ControlLeft','Ctrl','Ctrl'], ['AltLeft','Alt','Alt'], ['Space', ' '], ['AltRight', 'Alt','Alt'], ['ControlRight', 'Ctrl','Ctrl'],
+             ['ControlLeft','Ctrl','Ctrl'], ['AltLeft','Alt','Alt'], ['Space', '', ''], ['AltRight', 'Alt','Alt'], ['ControlRight', 'Ctrl','Ctrl'],
              ['ArrowLeft','left','left'], ['ArrowDown', 'down','down'], ['ArrowRight', 'right','right']
             ],
+        ru: [['Backquote', 'ё'], ['Digit1','1','!'], ['Digit2','2','"'], ['Digit3','3','№'], ['Digit4','4',';'], ['Digit5','5','%'],
+            ['Digit6','6',':'], ['Digit7','7','?'], ['Digit8','8','*'], ['Digit9','9','('], ['Digit0','0',')'],
+            ['Minus','-','_'], ['Equal','=','+'], ['Backspace','Backspace','Backspace'], ['Tab','Tab','Tab'], ['KeyQ','й'], ['KeyW','ц'],    
+            ['KeyE','у'], ['KeyR','к'], ['KeyT','е'], ['KeyY','н'], ['KeyU','г'], ['KeyI','ш'], ['KeyO','щ'],
+            ['KeyP','з'], ['BracketLeft','х'], ['BracketRight','ъ'], ['Backslash','\\','/'], ['Delete', 'Del','Del'],
+            ['CapsLock','Caps','Caps'], ['KeyA','ф'], ['KeyS','ы'], ['KeyD','в'], ['KeyF','а'], ['KeyG','п'], ['KeyH','р'],
+            ['KeyJ','о'], ['KeyK','л'], ['KeyL','д'], ['Semicolon','ж',], ['Quote','э',], ['Enter','Enter', 'Enter'],
+            ['ShiftLeft','Shift','Shift'], ['KeyZ','я'], ['KeyX','ч'],['KeyC','с'], ['KeyV','м'], ['KeyB','и'], ['KeyN','т'],
+            ['KeyM', 'ь'], ['Comma','б'], ['Period','ю'], ['Slash','.',','], ['ArrowUp','up','up'], ['ShiftRight', 'Shift','Shift'],
+            ['ControlLeft','Ctrl','Ctrl'], ['AltLeft','Alt','Alt'], ['Space', '', ''], ['AltRight', 'Alt','Alt'], ['ControlRight', 'Ctrl','Ctrl'],
+            ['ArrowLeft','left','left'], ['ArrowDown', 'down','down'], ['ArrowRight', 'right','right']
+           ],
+
+
         
       };
     
     this.specKeysFunctions = {
       ShiftLeft: {
-        down: () => {
-     
-          if(this.leftShiftPressed === 0){
-            this.leftShiftPressed = 1;
+        down: (evt) => {
+          this.setPressed(evt);
+          if(this.leftCtrlPressed) {
+            this.switchLang();
+          }else{
+            this.leftShiftPressed = true;
           }
-          
-          if(this.leftShiftPressed === 1){
-            this.retextKeys();
-          }
+          this.retextKeys();
         },
         
-        up: () => {
-
-          if(this.leftShiftPressed === 1){
-            this.leftShiftPressed = 0;
-          }
-
-          if(this.leftShiftPressed === 0){
-            this.retextKeys();
-          }
+        up: (evt) => {
+          this.setNoPressed(evt);
+          this.leftShiftPressed = false;
+          this.retextKeys();
         }
       },
       
       ShiftRight: {
-        down: () => {
-     
-          if(this.leftShiftPressed === 0){
-            this.leftShiftPressed = 1;
-          }
-          
-          if(this.leftShiftPressed === 1){
-            this.retextKeys();
-          }
+        down: (evt) => {
+          this.setPressed(evt);
+          this.leftShiftPressed = true;
+          this.retextKeys();
         },
         
-        up: () => {
-          
-          if(this.leftShiftPressed === 1){
-            this.leftShiftPressed = 0;
-          }
-
-          if(this.leftShiftPressed === 0){
-            this.retextKeys();
-          }
+        up: (evt) => {
+          this.setNoPressed(evt);
+          this.leftShiftPressed = false;
+          this.retextKeys();
         }
+      },
+
+      CapsLock: {
+        down: (evt) =>{
+          const vKey = document.getElementById(`Key_${evt.code}`);
+          this.caps = !this.caps;
+          if(this.caps){
+            vKey.classList.add('pressedKey');
+          }else{
+            vKey.classList.remove('pressedKey');
+          }
+          this.retextKeys();
+        },
+        up: (evt) => {
+
+        }
+      },
+
+      ControlLeft:{
+        down: (evt) => {
+          this.setPressed(evt);
+          this.leftCtrlPressed = true;
+        },
+        up: (evt) => {
+          this.setNoPressed(evt);
+          this.leftCtrlPressed = false;
+        }
+      },
+
+      Tab: {
+        down: (evt) => {
+          this.setPressed(evt);
+          evt.preventDefault();
+        },
+        
+        up: (evt) => {
+          this.setNoPressed(evt);
+          this.putCharToText('  ');
+          evt.preventDefault();
+        }
+      },
+      Enter: {
+        down: (evt) => {
+          this.setPressed(evt);
+        },
+        up: (evt) => {
+          this.setNoPressed(evt);
+        }
+      },
+      Space:{         
+        down: (evt) => {
+          this.setPressed(evt);
+        },
+        up: (evt) => {
+          this.setNoPressed(evt);
+        },
+        press: (evt) => {
+          this.putCharToText(' ');
+          evt.preventDefault();
+        }
+
       }
     }
+    
     this.makeDOM();
+  }
+
+  setPressed(evt){
+    const vKey = document.getElementById(`Key_${evt.code}`);
+    if(vKey){
+      vKey.classList.add('pressedKey');
+    }
+  }
+
+  setNoPressed(evt){
+    const vKey = document.getElementById(`Key_${evt.code}`);
+    if(vKey){
+      vKey.classList.remove('pressedKey');
+    }
   }
 
   makeDOM() {
     
     this.elText = this.addDomElement('textArea','myInputArea');
-    //this.elText.addEventListener('keydown',this.keyDownEventListener);
-    //this.elText.addEventListener('keyup',this.keyUpEventListener);
+    
     this.elText.addEventListener('keydown',this.keyDownEventListener.bind(this));
     this.elText.addEventListener('keyup',this.keyUpEventListener.bind(this));
+    this.elText.addEventListener('keypress',this.keyPressEventListener.bind(this));
     
     this.addDomElement('div','vKeyboard','vKeyboardId');
     
@@ -350,6 +430,8 @@ class cVKeyboard {
     let keyTexts = this.keyboardLangs[this.lang];
     let keyText;
     const shiftPressed =  this.leftShiftPressed || this.rightShiftPressed;    
+    const shifted = this.myXOR(shiftPressed, this.caps);
+    
     for(let i = 0; i< vKeys.length; i++){
       let keyCode = vKeys[i].id.split('_')[1];
       
@@ -358,36 +440,64 @@ class cVKeyboard {
         return false;
       });
 
-      vKeys[i].innerText = (keyText[1+shiftPressed])?keyText[1+shiftPressed] : 
-                            (shiftPressed == 1)? vKeys[i].innerText.toUpperCase() :vKeys[i].innerText.toLowerCase()  ;
+      vKeys[i].innerText = (keyText[1+shifted])?keyText[1+shifted] : 
+                            (shifted)? vKeys[i].innerText.toUpperCase() :vKeys[i].innerText.toLowerCase()  ;
     }
 
   }
   
+  switchLang(){
+    const langs = Object.keys(this.keyboardLangs);
+    let langIndex = langs.findIndex( item => item == this.lang);
+    if(++langIndex >= langs.length){
+      langIndex = 0;
+    } 
+    this.lang = langs[langIndex];
+    sessionStorage.setItem('vKeyboardLang',this.lang);
+  }
+
   keyDownEventListener(evt){
     const vKey = document.getElementById(`Key_${evt.code}`);
-    if(vKey){
-      vKey.classList.add('pressedKey');
+      
+    if ( (evt.code in this.specKeysFunctions) && ('down' in this.specKeysFunctions[evt.code]) ){
+        this.specKeysFunctions[evt.code].down(evt);
+    }else {
+      if(vKey){
+        vKey.classList.add('pressedKey');
+      }
     }
-    
-    if (evt.code in this.specKeysFunctions){
-      this.specKeysFunctions[evt.code].down();
-    }
-    //console.log(evt.code);
-    evt.preventDefault();
   }
 
   keyUpEventListener(evt){
     const vKey = document.getElementById(`Key_${evt.code}`);
-    if(vKey){
-      vKey.classList.remove('pressedKey');
-    }
 
-    if (evt.code in this.specKeysFunctions){
-      this.specKeysFunctions[evt.code].up();
+    if ((evt.code in this.specKeysFunctions) && ('up' in this.specKeysFunctions[evt.code])){
+        this.specKeysFunctions[evt.code].up(evt);
+    }else{
+      if(vKey){
+        vKey.classList.remove('pressedKey');
+      }
     }
-    evt.preventDefault();
-    
+  }
+
+  keyPressEventListener(evt){
+    if (!(evt.code in this.specKeysFunctions)){
+      const vKey = document.getElementById(`Key_${evt.code}`);
+      this.putCharToText(vKey.innerText);
+      evt.preventDefault();
+    }else{
+      if('press' in this.specKeysFunctions[evt.code]){
+        this.specKeysFunctions[evt.code].press(evt);
+      }
+    }  
+  }
+
+  putCharToText(char){
+    this.elText.value += char;
+  }
+
+  myXOR (a, b){
+    return ( a || b ) && !( a && b );
   }
 
 }
